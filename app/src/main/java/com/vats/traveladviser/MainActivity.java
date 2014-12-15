ebcd24a5ea74320836d3993a14402ca18cefe7ca
Kapil <kapl32.kumar@gmail.com>
2014-12-15 22:18:15 +0530

    Added tabs

--- app/src/main/java/com/vats/traveladviser/MainActivity.java
+++ app/src/main/java/com/vats/traveladviser/MainActivity.java
@@ -1,13 +1,23 @@
 package com.vats.traveladviser;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.vats.traveladviser.views.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private SlidingTabLayout mSlidingTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,12 @@ public class MainActivity extends ActionBarActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+
+        mViewPager = (ViewPager)findViewById(R.id.ViewPager);
+        mViewPager.setAdapter(new TabAdapter());
+
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
 
     }
 
@@ -37,5 +53,36 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class TabAdapter extends PagerAdapter{
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "item" + (position +1) ;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = getLayoutInflater().inflate(R.layout.pager_item, container, false);
+            container.addView(view);
+            TextView title = (TextView) view.findViewById(R.id.item_title);
+            title.setText(String.valueOf(position+1));
+            return  view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
